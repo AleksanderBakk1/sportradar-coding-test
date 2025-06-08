b4dd603945faf20b043c38a753cd19267c564bce
Aleksander Bakken <aleksander-bakken@hotmail.com>
2025-06-08 19:35:18 +0200

    Add tests for error cases

--- src/test/java/org/aleksander/sportradar/codingtest/objects/TeamTest.java
+++ src/test/java/org/aleksander/sportradar/codingtest/objects/TeamTest.java
@@ -1,8 +1,13 @@
 package org.aleksander.sportradar.codingtest.objects;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.security.InvalidParameterException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TeamTest {
     private static final String TEAM_NAME = "test_team_name";
@@ -15,5 +20,16 @@ public class TeamTest {
         assertEquals(TEAM_NAME, team.teamName());
         // And the toString() methods returns the team name when called
         assertEquals(TEAM_NAME, team.toString());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void attempting_to_create_a_team_with_null_or_empty_as_name_throws_exception(final String invalidTeamName) {
+        // Given null or empty team name, when creating team, it throws an exception
+        final InvalidParameterException invalidParameterException = assertThrows(
+                InvalidParameterException.class, () -> new Team(invalidTeamName)
+        );
+        // and the error message is descriptive
+        assertEquals("Parameter teamName cannot be null or empty!", invalidParameterException.getMessage());
     }
 }
